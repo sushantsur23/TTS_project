9c8e6b43c8d2aa557146794b7198d34e16c62855
sushantsur23 <sushant.sur23@gmail.com>
2023-02-02 16:32:40 +0530

    draft

--- src/stage_00.py
+++ src/stage_00.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+import subprocess
 
 
 STAGE = "Data Ingestion" ## <<< change stage name 
@@ -21,8 +22,25 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    
-    pass
+
+    text_file_path = config['data']["data_file"]
+
+    output_dir = config["data"]["output_dir"]
+    create_directories([output_dir])
+
+    output_file_name = config["data"]["file_name"]
+    output_file_path = os.path.join(output_dir, output_file_name)
+
+    if not os.path.isfile(output_file_path):
+        logging.info("downloading model started...")
+        cmd = "tts --text {text_file} --model_name 'tts_models/en/ljspeech/glow-tts' --out_path {output_file_path}"
+        subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    #     filename, headers = req.urlretrieve(URL, data_file_path)
+    #     logging.info(f"filename:{filename} created with info \n{headers}")
+    # else:
+    #     logging.info(f"filename:{data_file} already present")
+    # pass
 
 
 if __name__ == '__main__':
